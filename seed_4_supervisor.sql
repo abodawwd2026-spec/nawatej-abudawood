708da8bc4abfe0e45a3b90e5e69091bdbd712b0f
abodawwd2026-spec <abodawwd2026@gmail.com>
2026-09-15 17:42:45 +0300

    Delete seed_4_supervisor.sql

--- seed_4_supervisor.sql
+++
@@ -1,2 +0,0 @@
-INSERT OR REPLACE INTO settings(key,value) VALUES ('supervisor_username','admin');
-INSERT OR REPLACE INTO settings(key,value) VALUES ('supervisor_password_hash','30e1307a586a5fd658af3b1b5981f0a8:c0831e5ef1dc0316d8b26bd9dc64997c1c95f1699aa6f87d28d1111f6de07624');
